5d7e77d19a80238a9fe684ba54217d541db59cf4
Guilherme Mendonca <guigmen@gmail.com>
2018-03-29 18:48:39 -0300

    UDA-006 Add new API fetches
    feat: new Post & Comment fetches
    --------------------------------------------------
    
    - Add Create New Comment
    - Add Get All Comments from a Post
    - Add Delete Comment
    - Add Update Comment
    - Add Delete Post
    - Add Update Post

--- src/util/post.js
+++ src/util/post.js
@@ -16,15 +16,27 @@ export const votePost = (id, body) =>
     body: JSON.stringify(body)
   });
 
-export const getPostComments = id =>
-  fetch(`${CONSTANTS.BACKEND_URL}/posts/${id}/comments`, {
-    method: "GET",
-    headers: { Authorization: "whatever-you-want" }
-  });
-
 export const createNewPost = body =>
   fetch(`${CONSTANTS.BACKEND_URL}/posts`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "whatever-you-want"
+    },
+    body: JSON.stringify(body)
+  });
+
+export const deletePost = id =>
+  fetch(`${CONSTANTS.BACKEND_URL}/posts/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: "whatever-you-want"
+    }
+  });
+
+export const updatePost = (id, body) =>
+  fetch(`${CONSTANTS.BACKEND_URL}/posts/${id}`, {
+    method: "PUT",
     headers: {
       "Content-Type": "application/json",
       Authorization: "whatever-you-want"
